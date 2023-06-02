700c45473022a6b504ba8e8513aebf1d1b8aee46
Joakim <joakim_eriksson@msn.com>
2023-06-03 01:06:05 +0300

    feature/squash

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,8 +7,8 @@ package com.example;
 public class App {
     public interface hello {
         String k = "2";
-        int a = 1;
-        int b = 2;
+        int a = 2;
+        int b = 3;
         int c = 3;
     }
 
